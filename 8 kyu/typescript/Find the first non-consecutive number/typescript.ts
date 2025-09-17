026cdbda33a62eb34c5c107b85ab31b8394eb3da
Muhammad Hasan <muhammadhas772@gmail.com>
2025-09-17 11:41:35 +0800

    app: 8 kyu kata Find the first non-consecutive number for typescript

---
+++ 8 kyu/typescript/Find the first non-consecutive number/typescript.ts
@@ -0,0 +1,9 @@
+export const firstNonConsecutive = (arr: number[]): null | number => {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1] + 1) {
+      return arr[i];
+    }
+  }
+
+  return null;
+};
