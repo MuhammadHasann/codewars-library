7f07ec18ee665c623b1cbbd0e7ee6dec67812a86
Muhammad Hasan <muhammadhas772@gmail.com>
2025-11-01 13:05:28 +0800

    app: 8 kyu kata Drink about for typescript

---
+++ 8 kyu/typescript/Drink about/typescript.ts
@@ -0,0 +1,8 @@
+export const peopleWithAgeDrink = (old: number): string =>
+  old < 14
+    ? "drink toddy"
+    : old < 18
+    ? "drink coke"
+    : old < 21
+    ? "drink beer"
+    : "drink whisky";
